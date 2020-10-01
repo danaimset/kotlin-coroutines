094bd22db4a020bcca27aca8469078339b8e1f97
Daniil Lukashevich <danaimset@gmail.com>
2020-10-01 11:53:00 +0300

    5. Convert one-shot async requests to a coroutine

--- ktx-library-codelab/work/app/src/main/java/com/example/android/kotlinktxworkshop/MainActivity.kt
+++ ktx-library-codelab/work/app/src/main/java/com/example/android/kotlinktxworkshop/MainActivity.kt
@@ -22,14 +22,14 @@ import android.os.Bundle
 import android.os.Looper
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.android.myktxlibrary.createLocationRequest
-import com.example.android.myktxlibrary.findAndSetText
-import com.example.android.myktxlibrary.hasPermission
-import com.example.android.myktxlibrary.showLocation
+import androidx.lifecycle.lifecycleScope
+import com.example.android.myktxlibrary.*
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -58,18 +58,20 @@ class MainActivity : AppCompatActivity() {
             requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
         }
 
-        getLastKnownLocation()
-        startUpdatingLocation()
-    }
-
-    private fun getLastKnownLocation() {
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { lastLocation ->
-                showLocation(R.id.textView, lastLocation)
-            }.addOnFailureListener { e ->
+        lifecycleScope.launch {
+            try {
+                getLastKnownLocation()
+            } catch (e: Exception) {
                 findAndSetText(R.id.textView, "Unable to get location.")
                 Log.d(TAG, "Unable to get location", e)
             }
+        }
+        startUpdatingLocation()
+    }
+
+    private suspend fun getLastKnownLocation() {
+        val lastLocation = fusedLocationClient.awaitLastKnownLocation()
+        showLocation(R.id.textView, lastLocation)
     }
 
     private fun startUpdatingLocation() {
